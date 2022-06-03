77d90ad27293b047615c3d26fdf8b8670e6634af
Kimberly <kimolanga123@gmail.com>
2022-06-04 00:47:34 +0300

    update template folder

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index,name='index'),
+    path('new-post/', views.newPost,name='new-post'),
+
     
 ]
--- app/views.py
+++ app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def newPost(request):
+    return render(request,'new-post.html')
