0e5dea3d413b45526277e6a37e8fc0450c2773ef
Kimberly <kimolanga123@gmail.com>
2022-06-03 23:43:19 +0300

    add Image class

--- app/models.py
+++ app/models.py
@@ -1,4 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Image(models.Model):
+    image = CloudinaryField('image')
+    title = models.CharField(max_length=100)
+    image_caption = models.CharField(max_length=200)
+    profile = models.ForeignKey(User,on_delete=models.CASCADE)
+    likes = models.ManyToManyField('self', symmetrical = False, null = True)
+    comments = models.TextField(blank=True,null=True)
+
+    def __str__(self):
+        return self.image
