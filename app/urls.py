4b24592712f6e6e995bcc560d05259cc098af777
Kimberly <kimolanga123@gmail.com>
2022-06-04 16:00:49 +0300

    update new-post.html

--- app/urls.py
+++ app/urls.py
@@ -4,8 +4,9 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('login/',views.loginPage,name='login'),
     path('signup/',views.signupPage,name='signup'),
+    path('login/',views.loginPage,name='login'),
+    path('logout/',views.logoutUser,name='logout'),
     path('', views.index,name='index'),
     path('new-post/', views.newPost,name='new-post'),
 
--- app/views.py
+++ app/views.py
@@ -1,26 +1,56 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import ImageForm, CreateUserForm
 
 # Create your views here.
-def loginPage(request):
-    context = {}
-    return render(request,'accounts/login.html', context)
-
 def signupPage(request):
-    form = CreateUserForm()
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('login')
+    if request.user.is_authenticated:
+        return redirect('index')
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request,'Account was created for '+ user)
+                return redirect('login')
 
-    context = {'form':form}
-    return render(request,'accounts/signup.html', context)
+        context = {'form':form}
+        return render(request,'accounts/signup.html', context)
 
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request,username=username,password=password)
+
+            if user is not None:
+                login(request,user)
+                return redirect('index')
+            else:
+                messages.info(request,'username or password is incorrect')
+
+
+        context = {}
+        return render(request,'accounts/login.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+login_required(login_url='login')
 def index(request):
     return render(request,'index.html')
 
+login_required(login_url='login')
 def newPost(request):
     return render(request,'new-post.html')
