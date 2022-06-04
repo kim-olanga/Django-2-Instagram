e3155fc5b270b8ab175dcc804a2265e075d9696b
Kimberly <kimolanga123@gmail.com>
2022-06-04 12:58:03 +0300

    import forms

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,13 @@
 from dataclasses import fields
 from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
 from .models import Image
+
 
 class ImageForm(ModelForm):
    class Meta:
        model = Image
        fields = '__all__' 
+
+    
