da47a13cabdad69310c855d3309bf1c767247c52
Kimberly <kimolanga123@gmail.com>
2022-06-04 12:56:34 +0300

    add meta class

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,8 @@
+from dataclasses import fields
 from django.forms import ModelForm
 from .models import Image
 
 class ImageForm(ModelForm):
-    
+   class Meta:
+       model = Image
+       fields = '__all__' 
